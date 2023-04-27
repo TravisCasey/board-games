0d86fb4bea72c852818a3f6006fddf4104ac4a05
Travis Casey <traviscasey14@outlook.com>
2023-04-27 14:31:21 -0500

    Formatting

--- pyboardgames/games/template.py
+++ pyboardgames/games/template.py
@@ -12,11 +12,11 @@ from abc import ABC, abstractmethod
 
 
 class GamestateTemplate(ABC):
-    
+
     @abstractmethod
     def update(self, move):
         """Updates the gamestate according to the provided move.
-        
+
         Args:
             move: An instance of a subclass of the MoveTemplate abstract
                 class created for the game being played.
@@ -28,20 +28,20 @@ class GamestateTemplate(ABC):
         """This property is a collection of all legal moves.
 
         Should be implemented with the property decorator.
-        
+
         Returns:
             A list of instances of the proper subclass of the
             Template abstract class defined for the game being played,
             that are legal moves for the current player to make.
         """
         pass
-    
+
     @abstractmethod
     def winner(self):
         """Determines which player won the game, if any.
 
         Should be implemented with the property decorator.
-        
+
         Returns:
         1: Team 1 won.
         -1: Team 2 won.
@@ -53,18 +53,18 @@ class GamestateTemplate(ABC):
     @abstractmethod
     def is_game_over(self):
         """Indicates if the game is over.
-        
+
         Returns:
         True: game has ended.
         False: game has not ended.
         """
         pass
-    
+
     @abstractmethod
     def copy(self):
         """Returns a deep copy of the currnet state.
-        
-        Returns: 
+
+        Returns:
             An identical but distinct object as the instance with this
             method.
         """

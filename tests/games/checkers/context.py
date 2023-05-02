adee34a71c99152d95bc7652fd77cb7a0d16a955
Travis Casey <traviscasey14@outlook.com>
2023-05-02 10:36:24 -0500

    Added Docstring.

--- tests/games/checkers/context.py
+++ tests/games/checkers/context.py
@@ -1,3 +1,5 @@
+"""Handles imports for the test package."""
+
 import sys
 import os
 
@@ -5,3 +7,4 @@ sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..\\..\\..')))
 
 from pyboardgames.games.checkers import game
+from pyboardgames.games.checkers import gui

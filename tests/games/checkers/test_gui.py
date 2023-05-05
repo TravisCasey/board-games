a5654c8dfb972af761eb0364f5a217fd853c397c
Travis Casey <traviscasey14@outlook.com>
2023-05-05 13:15:23 -0500

    Wrote testing script for gui.

--- tests/games/checkers/test_gui.py
+++ tests/games/checkers/test_gui.py
@@ -1,3 +1,9 @@
-from context import gui
+from context import gui, RandomAgent
+import sys
 
-gui.CheckersGUI()
+match sys.argv[1]:
+    case '0': gui.CheckersGUI()
+    case '1': gui.CheckersGUI(player1=RandomAgent())
+    case '2': gui.CheckersGUI(player2=RandomAgent())
+    case '3': gui.CheckersGUI(player1=RandomAgent(),
+                              player2=RandomAgent())
